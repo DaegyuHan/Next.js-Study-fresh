81fc3f27df0dbb0b8496978e6fe45afd8b5a5928
Daegyu Han <bigstar77han@gmail.com>
2025-02-09 19:39:31 +0900

    routing, layout.js

---
+++ app/cart/page.js
@@ -0,0 +1,9 @@
+export default function Cart() {
+
+    return (
+      <div>
+        <h1 className="title">장바구니</h1>
+      </div>
+    );
+  }
+  
---
+++ app/cart/payment/page.js
@@ -0,0 +1,9 @@
+export default function Cart() {
+
+    return (
+      <div>
+        <h1 className="title">결제페이지</h1>
+      </div>
+    );
+  }
+  
