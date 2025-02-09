ab6dde2c7ae6c0e103377617274d3afad104eeb4
Daegyu Han <bigstar77han@gmail.com>
2025-02-10 01:28:11 +0900

    client/server component, import

---
+++ app/cart/data.js
@@ -0,0 +1,3 @@
+let age = 29;
+
+export default age;
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,24 @@
+import age from './data.js'
+import Hi from './hi.js'
+
 export default function Cart() {
     return (
-      <div>
-        <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
+        <div>
+            <Hi/>
+            <h4 className="title">Cart</h4>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
         </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-      </div>
     )
-  } 
+}
+
+function CartItem() {
+    return (
+        <div className="cart-item">
+            <p>상품명 {age}</p>
+            <p>$40</p>
+            <p>1개</p>
+        </div>
+    )
+}
