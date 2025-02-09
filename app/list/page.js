244b805b44f327e7b497920aad4f418b50e86a33
Daegyu Han <bigstar77han@gmail.com>
2025-02-09 23:20:40 +0900

    map function

--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,18 @@
 export default function List() {
-
+    let products = ['Tomatoes', 'Pasta', 'Coconut']
     return (
-      <div>
-        <h4 className="title">상품 목록</h4>
-        <div className="food">
-            <h4>상품1 $40</h4>
+        <div>
+            <h4 className="title">상품 목록</h4>
+            {
+                products.map((product, i) => {
+                    return (
+                        <div className="food" key={i}>
+                            
+                            <h4>{products[i]} $40</h4>
+                        </div>
+                    )
+                })
+            }
         </div>
-        <div className="food">
-            <h4>상품2 $40</h4>
-        </div>
-      </div>
     );
-  }
-  
+}
