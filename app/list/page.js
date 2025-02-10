3ac5fa0be81d2f3fa4efd3d6ca06451edc798ca3
Daegyu Han <bigstar77han@gmail.com>
2025-02-10 23:49:44 +0900

    edit array, object state

--- app/list/page.js
+++ app/list/page.js
@@ -2,8 +2,23 @@
 import { useState } from "react";
 
 export default function List() {
-    let [count, changeCount] = useState(0)
+    let [count, changeCount] = useState([0, 0, 0])
     let products = ['Tomatoes', 'Pasta', 'Coconut']
+
+    // 수량 감소 함수
+    const decreaseCount = (index) => {
+        let copy = [...count];
+        copy[index]--;
+        changeCount(copy);
+    }
+
+    // 수량 증가 함수
+    const increaseCount = (index) => {
+        let copy = [...count];
+        copy[index]++;
+        changeCount(copy);
+    }
+
     return (
         <div>
             <h4 className="title">상품 목록</h4>
@@ -13,13 +28,14 @@ export default function List() {
                         <div className="food" key={i}>
                             <img src={`/food${i}.png`} className="food-img"></img>
                             <h4>{products[i]} $40</h4>
-                            <button onClick={()=>{changeCount(count-1)}}>-</button>
-                            <span>{count}</span>
-                            <button onClick={()=>{changeCount(count+1)}}>+</button>
+                            <button onClick={() => { decreaseCount(i)}}>-</button>
+                            <span>{count[i]}</span>
+                            <button onClick={() => {increaseCount(i)}}>+</button>
                         </div>
                     )
                 })
             }
         </div>
     );
+
 }
